3bbcbf56fd9d0aefaf79d1a22afea0d8aa16c0fc
DavidSC <david@wbcboxing.com>
2021-10-11 15:35:58 -0500

    create conntroller under api folder

---
+++ app/Controllers/Api/ApiController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controllers\Api;
+
+use CodeIgniter\RESTful\ResourceController;
+
+class ApiController extends ResourceController
+{
+    //POST
+    public function creatCategory()
+    {
+    }
+
+    //GET
+    public function listCategory()
+    {
+    }
+    //POST
+    public function createBlog()
+    {
+    }
+
+    //GET
+    public function listBlogs()
+    {
+    }
+    //GET
+    public function singleBlogDetail($blog_id)
+    {
+    }
+    //POSt --> PUT
+    public function  updateBlog($blog_id)
+    {
+    }
+    //DELETe
+    public function deleteBlog($blog_id)
+    {
+    }
+}
